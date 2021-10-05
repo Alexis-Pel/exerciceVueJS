a90c6397190103eeb0996d0759c23de39b4503b5
Alexis Pelissier <apelissier@MacBook-Pro-de-BG.local>
2021-10-05 15:41:26 +0200

    enleve ex 1 et 2

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
-    <Horloge />
-    <Compteur />
     <Liste />
     <ListDone />
   </div>
 </template>
 
 <script>
-import Compteur from "./components/Compteur";
 import Liste from "./components/Liste";
-import Horloge from "./components/Horloge";
 import ListDone from "./components/ListDone";
 
 export default {
   name: "App",
   components: {
-    Compteur,
     Liste,
-    Horloge,
     ListDone
   },
 };
